8a9108118937c1e5be08d7ab25239cbbff8eca7c
Jamie <elliott1234@users.noreply.github.com>
2018-03-24 12:40:01 +0000

    first commit

---
+++ resources/js/match-game.js
@@ -0,0 +1,12 @@
+var MatchGame = {};
+
+/*
+  Sets up a new game after HTML document has loaded.
+  Renders a 4x4 board of cards.
+*/
+
+
+
+/*
+  Generates and returns an array of matching card values.
+ */
